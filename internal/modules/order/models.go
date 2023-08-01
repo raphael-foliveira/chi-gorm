e07729e327309fedeaa124d4e5bc1be770b16cf6
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-08-01 19:27:18 -0300

    remove faker tags from order model

--- internal/modules/order/models.go
+++ internal/modules/order/models.go
@@ -9,13 +9,13 @@ import (
 )
 
 type Order struct {
-	ID        uint            `json:"id" gorm:"primarykey" faker:"-"`
-	CreatedAt time.Time       `json:"-" faker:"-"`
-	UpdatedAt time.Time       `json:"-" faker:"-"`
-	DeletedAt sql.NullTime    `gorm:"index" json:"-" faker:"-"`
-	ClientID  uint            `json:"-" faker:"-"`
-	Client    client.Client   `json:"client" faker:"-"`
-	ProductID uint            `json:"-" faker:"-"`
-	Product   product.Product `json:"product" faker:"-"`
-	Quantity  uint            `json:"quantity" faker:"-"`
+	ID        uint            `json:"id" gorm:"primarykey"`
+	CreatedAt time.Time       `json:"-"`
+	UpdatedAt time.Time       `json:"-"`
+	DeletedAt sql.NullTime    `gorm:"index" json:"-"`
+	ClientID  uint            `json:"-"`
+	Client    client.Client   `json:"client"`
+	ProductID uint            `json:"-"`
+	Product   product.Product `json:"product"`
+	Quantity  uint            `json:"quantity"`
 }
