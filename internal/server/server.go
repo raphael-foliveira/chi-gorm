df5cd765d343b31d1980ea2bcbf40e70284ab07a
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-08-28 23:09:34 -0300

    chore: formating

--- internal/server/server.go
+++ internal/server/server.go
@@ -24,19 +24,17 @@ func attachMiddleware(r *chi.Mux) {
 func mountRouters(r *chi.Mux, db *db.DB) {
 	clientsRepository := client.NewRepository(db)
 	clientsRouter := client.NewRouter(clientsRepository)
-
 	productsRepository := product.NewRepository(db)
 	productsRouter := product.NewRouter(productsRepository)
-
 	ordersRepository := order.NewRepository(db)
 	ordersRouter := order.NewRouter(ordersRepository)
-
 	r.Mount("/clients", clientsRouter)
 	r.Mount("/products", productsRouter)
 	r.Mount("/orders", ordersRouter)
 }
 
 func Start(db *db.DB) error {
+	db.AutoMigrate(&client.Client{}, &product.Product{}, &order.Order{})
 	mainRouter := chi.NewRouter()
 	attachMiddleware(mainRouter)
 	mainRouter.Get("/", func(w http.ResponseWriter, r *http.Request) {
