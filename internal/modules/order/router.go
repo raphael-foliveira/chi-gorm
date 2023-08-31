146b1a0906dcbe24b821850b3539d575cc436a72
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-08-31 18:53:11 -0300

    refactor: mount json middleware in main router

--- internal/modules/order/router.go
+++ internal/modules/order/router.go
@@ -2,18 +2,14 @@ package order
 
 import (
 	"github.com/go-chi/chi/v5"
-	"github.com/raphael-foliveira/chi-gorm/pkg/middleware"
 )
 
 func NewRouter(controller *Controller) *chi.Mux {
-
-	ordersRouter := chi.NewRouter()
-	ordersRouter.Use(middleware.Json)
-
-	ordersRouter.Get("/", controller.List)
-	ordersRouter.Post("/", controller.Create)
-	ordersRouter.Get("/{id}", controller.Get)
-	ordersRouter.Delete("/{id}", controller.Delete)
-	ordersRouter.Put("/{id}", controller.Update)
-	return ordersRouter
+	router := chi.NewRouter()
+	router.Get("/", controller.List)
+	router.Post("/", controller.Create)
+	router.Get("/{id}", controller.Get)
+	router.Delete("/{id}", controller.Delete)
+	router.Put("/{id}", controller.Update)
+	return router
 }
--- internal/modules/product/router.go
+++ internal/modules/product/router.go
@@ -2,16 +2,14 @@ package product
 
 import (
 	"github.com/go-chi/chi/v5"
-	"github.com/raphael-foliveira/chi-gorm/pkg/middleware"
 )
 
 func NewRouter(controller *Controller) *chi.Mux {
-	productRouter := chi.NewRouter()
-	productRouter.Use(middleware.Json)
-	productRouter.Get("/", controller.List)
-	productRouter.Post("/", controller.Create)
-	productRouter.Get("/{id}", controller.Get)
-	productRouter.Delete("/{id}", controller.Delete)
-	productRouter.Put("/{id}", controller.Update)
-	return productRouter
+	router := chi.NewRouter()
+	router.Get("/", controller.List)
+	router.Post("/", controller.Create)
+	router.Get("/{id}", controller.Get)
+	router.Delete("/{id}", controller.Delete)
+	router.Put("/{id}", controller.Update)
+	return router
 }
