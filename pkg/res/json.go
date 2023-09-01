bdcc6d4199bd771f34626f3a9c72a1465a2c4700
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-09-01 07:53:56 -0300

    refactor: remove set header call since middleware is already handling this

--- pkg/res/json.go
+++ pkg/res/json.go
@@ -8,7 +8,6 @@ import (
 type M map[string]interface{}
 
 func JSON(w http.ResponseWriter, status int, data interface{}) error {
-	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	return json.NewEncoder(w).Encode(data)
 }
