0e3074cde783ac906a84f464034d4a18cfd8d0ee
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-09-16 16:38:57 -0300

    tests: move controller tests to individual files

---
+++ pkg/controllers/utils_test.go
@@ -0,0 +1,16 @@
+package controllers
+
+import (
+	"net/http/httptest"
+	"testing"
+)
+
+func TestUtils(t *testing.T) {
+	t.Run("Should return an error when given an invalid id", func(t *testing.T) {
+		request := httptest.NewRequest("GET", "/foo", nil)
+		_, err := getIdFromPath(request)
+		if err == nil {
+			t.Error("Should return an error")
+		}
+	})
+}
