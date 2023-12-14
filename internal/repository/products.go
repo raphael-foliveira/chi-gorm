8f9f604eabdc0a4c2b9177e974c1ad684067cc60
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-12-14 11:13:56 -0300

    refactor: clean products repository

--- internal/repository/products.go
+++ internal/repository/products.go
@@ -19,28 +19,6 @@ func NewProducts(db *gorm.DB) Products {
 	return &products{newRepository[entities.Product](db)}
 }
 
-func (r *products) List() ([]entities.Product, error) {
-	c := []entities.Product{}
-	return c, r.db.Find(&c).Error
-}
-
-func (r *products) Get(id int64) (*entities.Product, error) {
-	product := entities.Product{}
-	return &product, r.db.First(&product, id).Error
-}
-
-func (r *products) Create(product *entities.Product) error {
-	return r.db.Create(product).Error
-}
-
-func (r *products) Update(product *entities.Product) error {
-	return r.db.Save(product).Error
-}
-
-func (r *products) Delete(product *entities.Product) error {
-	return r.db.Delete(product).Error
-}
-
 func (r *products) FindMany(ids []int64) ([]entities.Product, error) {
 	products := []entities.Product{}
 	return products, r.db.Find(&products, ids).Error
