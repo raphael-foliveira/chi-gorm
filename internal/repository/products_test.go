199fb7f49de51a224c6dafe3418b82883f4d6ea7
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-12-30 08:56:29 -0300

    tests: remove random data from repository test since it was leading to random failures

--- internal/repository/products_test.go
+++ internal/repository/products_test.go
@@ -2,14 +2,24 @@ package repository
 
 import (
 	"testing"
+
+	"github.com/raphael-foliveira/chi-gorm/internal/database"
+	"github.com/raphael-foliveira/chi-gorm/internal/entities"
 )
 
 func TestProductsRepository(t *testing.T) {
 	t.Run("Should find many", func(t *testing.T) {
-		products, err := addProducts(2)
-		if err != nil {
-			t.Error(err)
+		products := []entities.Product{
+			{
+				Name:  "Brand 1",
+				Price: 1.0,
+			},
+			{
+				Name:  "Brand 2",
+				Price: 2.0,
+			},
 		}
+		database.Db.Create(&products)
 		foundProducts, err := Products.FindMany([]uint{products[0].ID, products[1].ID})
 		if err != nil {
 			t.Error(err)
--- internal/repository/repository_test.go
+++ internal/repository/repository_test.go
@@ -3,10 +3,8 @@ package repository
 import (
 	"testing"
 
-	"github.com/go-faker/faker/v4"
 	"github.com/raphael-foliveira/chi-gorm/internal/cfg"
 	"github.com/raphael-foliveira/chi-gorm/internal/database"
-	"github.com/raphael-foliveira/chi-gorm/internal/entities"
 )
 
 func TestMain(m *testing.M) {
@@ -23,16 +21,4 @@ func TestMain(m *testing.M) {
 	if err != nil {
 		panic(err)
 	}
-}
-
-func addProducts(quantity int) ([]entities.Product, error) {
-	products := []entities.Product{}
-	err := faker.FakeData(&products)
-	if err != nil {
-		return nil, err
-	}
-	for i := range products {
-		products[i].ID = 0
-	}
-	return products, database.Db.Create(&products).Error
 }
