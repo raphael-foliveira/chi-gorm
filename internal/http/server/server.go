099de7724c16fe90d837c38f286f302ec1a85e18
Raphael Oliveira <raphael.foliveira1@gmail.com>
2024-02-20 20:25:39 -0300

    refactor: rename self reference

--- internal/http/server/server.go
+++ internal/http/server/server.go
@@ -22,36 +22,36 @@ func NewApp(db *gorm.DB) *App {
 	return &App{db}
 }
 
-func (s *App) Start() error {
-	app := s.CreateRouter()
+func (a *App) Start() error {
+	app := a.CreateRouter()
 	slog.Info("listening on port 3000")
 	return http.ListenAndServe(":3000", app)
 }
 
-func (s *App) CreateRouter() *chi.Mux {
+func (a *App) CreateRouter() *chi.Mux {
 	mainRouter := chi.NewRouter()
-	s.attachMiddleware(mainRouter)
-	controllers := s.injectDependencies()
-	s.mountRoutes(mainRouter, controllers)
+	a.attachMiddleware(mainRouter)
+	controllers := a.injectDependencies()
+	a.mountRoutes(mainRouter, controllers)
 	return mainRouter
 }
 
-func (s *App) injectDependencies() *routes.Routers {
-	repositories := repository.NewRepositories(s.db)
+func (a *App) injectDependencies() *routes.Routers {
+	repositories := repository.NewRepositories(a.db)
 	services := service.NewServices(repositories)
 	controllers := controller.NewControllers(services)
 	routes := routes.NewRouters(controllers)
 	return routes
 }
 
-func (s *App) mountRoutes(r *chi.Mux, rts *routes.Routers) {
+func (a *App) mountRoutes(r *chi.Mux, rts *routes.Routers) {
 	r.Mount("/clients", rts.Clients)
 	r.Mount("/products", rts.Products)
 	r.Mount("/orders", rts.Orders)
 	r.Get("/health-check", rts.HealthCheck)
 }
 
-func (s *App) attachMiddleware(r *chi.Mux) {
+func (a *App) attachMiddleware(r *chi.Mux) {
 	r.Use(middleware.Logger)
 	r.Use(cors.Handler(cors.Options{
 		AllowedOrigins: []string{"*"},
