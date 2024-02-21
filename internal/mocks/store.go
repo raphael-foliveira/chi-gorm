c5ad0788553db6c802b817782ee0b933c3c7babf
Raphael Oliveira <raphael.foliveira1@gmail.com>
2024-02-20 21:34:39 -0300

    refactor: move store mocks to the same file

--- internal/mocks/store.go
+++ internal/mocks/store.go
@@ -48,3 +48,43 @@ func (s *store[T]) Delete(client *T) error {
 	}
 	return errors.New("not found")
 }
+
+var ClientsStore = &clientsStore{store[entities.Client]{}}
+
+type clientsStore struct {
+	store[entities.Client]
+}
+
+var ProductsStore = &productsStore{store[entities.Product]{}}
+
+type productsStore struct {
+	store[entities.Product]
+}
+
+func (cr *productsStore) FindMany(ids []uint) ([]entities.Product, error) {
+	products := []entities.Product{}
+	for _, id := range ids {
+		for _, product := range cr.Store {
+			if product.ID == id {
+				products = append(products, product)
+			}
+		}
+	}
+	return products, cr.Error
+}
+
+var OrdersStore = &ordersStore{store[entities.Order]{}}
+
+type ordersStore struct {
+	store[entities.Order]
+}
+
+func (os *ordersStore) FindManyByClientId(clientId uint) ([]entities.Order, error) {
+	orders := []entities.Order{}
+	for _, order := range os.Store {
+		if order.ClientID == clientId {
+			orders = append(orders, order)
+		}
+	}
+	return orders, os.Error
+}
