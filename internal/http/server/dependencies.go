7e4ce3393291b09ba510d0ca30a01b51fcc41b5b
Raphael Oliveira <raphael.foliveira1@gmail.com>
2024-03-06 15:43:47 -0300

    chore: formatting

--- internal/http/server/dependencies.go
+++ internal/http/server/dependencies.go
@@ -12,36 +12,47 @@ import (
 func clientsRoutes() *chi.Mux {
 	return routes.Clients(clientsController())
 }
+
 func clientsController() *controller.Clients {
 	return controller.NewClients(clientsService())
 }
+
 func clientsService() *service.Clients {
 	return service.NewClients(clientsRepository(), ordersRepository())
 }
-func clientsRepository() *repository.Clients {
-	return repository.NewClients(database.Db())
-}
+
 func ordersRepository() *repository.Orders {
 	return repository.NewOrders(database.Db())
 }
+
 func productsRepository() *repository.Products {
 	return repository.NewProducts(database.Db())
 }
+
 func productsService() *service.Products {
 	return service.NewProducts(productsRepository())
 }
+
 func productsController() *controller.Products {
 	return controller.NewProducts(productsService())
 }
+
 func productsRoutes() *chi.Mux {
 	return routes.Products(productsController())
 }
+
+func clientsRepository() *repository.Clients {
+	return repository.NewClients(database.Db())
+}
+
 func ordersService() *service.Orders {
 	return service.NewOrders(ordersRepository())
 }
+
 func ordersController() *controller.Orders {
 	return controller.NewOrders(ordersService())
 }
+
 func ordersRoutes() *chi.Mux {
 	return routes.Orders(ordersController())
 }
