b8c347143cecded622e3c883d8d33919c4e725b4
Raphael Oliveira <raphael.foliveira1@gmail.com>
2024-03-07 11:27:27 -0300

    refactor: move common routes functionality to routes.go file

--- internal/http/routes/routes.go
+++ internal/http/routes/routes.go
@@ -1,7 +1,14 @@
 package routes
 
 import (
+	"errors"
+	"log/slog"
+	"net/http"
+
 	"github.com/go-chi/chi/v5"
+	"github.com/raphael-foliveira/chi-gorm/internal/exceptions"
+	"github.com/raphael-foliveira/chi-gorm/internal/http/res"
+	"github.com/raphael-foliveira/chi-gorm/internal/validate"
 )
 
 type router struct {
@@ -26,4 +33,29 @@ func (r *router) Patch(path string, fn ControllerFunc) {
 
 func (r *router) Delete(path string, fn ControllerFunc) {
 	r.Mux.Delete(path, useHandler(fn))
+}
+
+type ControllerFunc func(w http.ResponseWriter, r *http.Request) error
+
+func useHandler(fn ControllerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := fn(w, r)
+		if err != nil {
+			handleApiErr(w, err)
+		}
+	}
+}
+
+func handleApiErr(w http.ResponseWriter, err error) error {
+	slog.Error(err.Error())
+	apiErr := &exceptions.ApiError{
+		Status:  http.StatusInternalServerError,
+		Message: "internal server error",
+	}
+	validationErr := &validate.ValidationError{}
+	if errors.As(err, &validationErr) {
+		return res.JSON(w, http.StatusUnprocessableEntity, validationErr)
+	}
+	errors.As(err, &apiErr)
+	return res.JSON(w, apiErr.Status, apiErr)
 }
