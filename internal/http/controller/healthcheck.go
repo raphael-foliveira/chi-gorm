1dc99f1db99b9d71d2ebbd5ce6c3e17c9b8ba9f7
Raphael Oliveira <raphael.foliveira1@gmail.com>
2025-03-22 16:57:05 -0300

    refactor: healthcheck mount

--- internal/http/controller/healthcheck.go
+++ internal/http/controller/healthcheck.go
@@ -12,17 +12,13 @@ func (h *HealthCheck) healthCheck(ctx *Context) error {
 	return ctx.JSON(http.StatusOK, map[string]string{"status": "ok"})
 }
 
-func (c *HealthCheck) Routes() *chi.Mux {
-	router := NewRouter()
-	router.Get("/", c.healthCheck)
-
-	return router.Mux
-}
-
 func NewHealthCheck() *HealthCheck {
 	return &HealthCheck{}
 }
 
 func (h *HealthCheck) Mount(mux *chi.Mux) {
-	mux.Mount("/health-check", h.Routes())
+	router := NewRouter()
+	router.Get("/", h.healthCheck)
+
+	mux.Mount("/health-check", router)
 }
