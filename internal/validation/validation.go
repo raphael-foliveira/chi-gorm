5e69791cd6734b110f52b991cd603722d0f2dc7e
Raphael Oliveira <raphael.foliveira1@gmail.com>
2025-07-19 11:27:43 -0300

    refactor: validation package

---
+++ internal/validation/validation.go
@@ -0,0 +1,12 @@
+package validation
+
+func Validate(validateFn func(v *Validator)) map[string][]string {
+	vv := NewValidator()
+	validateFn(vv)
+
+	if !vv.IsValid() {
+		return nil
+	}
+
+	return vv.Errors
+}
